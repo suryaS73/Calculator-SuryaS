dd418fbd4901af82840982f0003a1bfebaf5522b
suryaS73 <125781536+suryaS73@users.noreply.github.com>
2023-02-24 12:53:33 +0530

    Add files via upload

---
+++ Calculator/JavaScript/calculator.js
@@ -0,0 +1,60 @@
+function on()
+{
+     var one=new Audio()
+     one.src=("../Assets/1.mp3")
+     one.play()
+}
+function zero()
+{
+     var zero=new Audio()
+     zero.src=("../Assets/0.mp3")
+     zero.play()
+}
+function tw()
+{
+     var two=new Audio()
+     two.src=("../Assets/2.mp3")
+     two.play()
+}
+function th()
+{
+     var three=new Audio()
+     three.src=("../Assets/3.mp3")
+     three.play()
+}
+function fou()
+{
+     var four=new Audio()
+     four.src=("../Assets/4.mp3")
+     four.play()
+}
+function fi()
+{
+     var five=new Audio()
+     five.src=("../Assets/5.mp3")
+     five.play()
+}
+function si()
+{
+     var six=new Audio()
+     six.src=("../Assets/6.mp3")
+     six.play()
+}
+function se()
+{
+     var seven=new Audio()
+     seven.src=("../Assets/7.mp3")
+     seven.play()
+}
+function ei()
+{
+     var eight=new Audio()
+     eight.src=("../Assets/8.mp3")
+     eight.play()
+}
+function ni()
+{
+     var nine=new Audio()
+     nine.src=("../Assets/9.mp3")
+     nine.play()
+}
