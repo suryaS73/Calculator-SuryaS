1a059f568a6712d7dd56dd0dbbcae60d2cdba0a4
suryaS73 <125781536+suryaS73@users.noreply.github.com>
2023-02-24 14:24:45 +0530

    Add files via upload

---
+++ JavaScript/calculator.js
@@ -0,0 +1,66 @@
+function on()
+{
+     var one=new Audio()
+     one.src=("./Assets/Audio/1.mp3")
+     one.play()
+}
+function zero()
+{
+     var zero=new Audio()
+     zero.src=("./Assets/Audio/0.mp3")
+     zero.play()
+}
+function tw()
+{
+     var two=new Audio()
+     two.src=("./Assets/Audio/2.mp3")
+     two.play()
+}
+function th()
+{
+     var three=new Audio()
+     three.src=("./Assets/Audio/3.mp3")
+     three.play()
+}
+function fou()
+{
+     var four=new Audio()
+     four.src=("./Assets/Audio/4.mp3")
+     four.play()
+}
+function fi()
+{
+     var five=new Audio()
+     five.src=("./Assets/Audio/5.mp3")
+     five.play()
+}
+function si()
+{
+     var six=new Audio()
+     six.src=("./Assets/Audio/6.mp3")
+     six.play()
+}
+function se()
+{
+     var seven=new Audio()
+     seven.src=("./Assets/Audio/7.mp3")
+     seven.play()
+}
+function ei()
+{
+     var eight=new Audio()
+     eight.src=("./Assets/Audio/8.mp3")
+     eight.play()
+}
+function ninee()
+{
+     var nin=new Audio()
+     nin.src=("./Assets/Audio/9.mp3")
+     nin.play()
+}
+function clicksound()
+{
+     var click= new Audio()
+     click.src=("./Assets/Audio/click.wav")
+     click.play()
+}
